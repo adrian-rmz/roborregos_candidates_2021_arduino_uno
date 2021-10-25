5daf27b6a30af3f847284e784b1ca0f88caac8ea
ale058791 <ale058791@gmail.com>
2021-10-25 14:16:44 -0500

    Implementation of Color Sensor and RGB LED

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ int IN3 = 13;
 int IN4 = 12;
 int ENB = 11;
 
+
 // Front Ultrasonic
 int trigger_front = A0;
 int echo_front = A1;
@@ -23,10 +24,32 @@ int echo_right = A3;
 int trigger_left = A4;
 int echo_left = A5;
 
+
 // Speeds 
 int min_speed = 85;
 int mid_speed = 170;
 int max_speed = 255;
+
+
+// TCS3200 Color Sensor
+int s2 = 2;
+int s3 = 3;
+int out_pin = 4;
+
+// Color Variables
+int r_color;
+int g_color;
+int b_color;
+
+// RGB LED
+int red_pin = 14;
+int green_pin = 15;
+int blue_pin = 16;
+
+
+
+// PULSE WIDTH
+unsigned int pulse_width;
 
 
 void setup() {
@@ -38,6 +61,16 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   pinMode(ENB, OUTPUT);
+
+  // TCS3200 pinModes
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+  pinMode(out_pin, INPUT);
+
+  // RGB LED pinModes
+  pinMode(red_pin, OUTPUT);
+  pinMode(green_pin, OUTPUT);
+  pinMode(blue_pin, OUTPUT);
 
   // Serial Communication
   Serial.begin(9600);
@@ -142,4 +175,83 @@ float distance_left() {
   float duration_left = pulseIn(echo_left, HIGH);
   float distance_left = duration_left * 0.34 / 2;
   return distance_left;
+}
+
+
+//***********************COLOR SENSOR***********************//
+
+void color_sensor() {
+  // RED
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, LOW);
+  pulse_width = pulseIn(out_pin, LOW);
+  r_color = pulse_width / 400. - 1;
+  r_color = (255 - r_color);
+
+  // GREEN
+  digitalWrite(s2, HIGH);
+  digitalWrite(s3, HIGH);
+  pulse_width = pulseIn(out_pin, LOW);
+  g_color = pulse_width / 400. - 1;
+  g_color = (255 - g_color);
+
+  // BLUE
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, HIGH);
+  pulse_width = pulseIn(out_pin, LOW);
+  b_color = pulse_width / 400. - 1;
+  b_color = (255 - b_color);
+
+  // TEST
+  Serial.print(r_color);
+  Serial.print(" , ");
+  Serial.print(g_color);  
+  Serial.print(" , ");  
+  Serial.print(b_color);
+  Serial.println("");
+
+    
+  // RGB LED
+  if(r_color > g_color && g_color > b_color){
+    r_color = 255;
+    g_color = g_color / 2;
+    b_color = 0;
+  } else if (r_color > b_color && b_color > g_color){
+    r_color = 255;
+    g_color = 0;
+    b_color = b_color / 2;
+  } else if (g_color > r_color && r_color > b_color){
+    r_color = r_color / 2;
+    g_color = 255;
+    b_color = 0;
+  } else if (g_color > b_color && b_color > r_color){
+    r_color = 0;
+    g_color = 255;
+    b_color = b_color / 2;
+  } else if (b_color > r_color && r_color > g_color){
+    r_color = r_color / 2;
+    g_color = 0;
+    b_color = 255;
+  } else if (b_color > g_color && g_color > r_color){
+    r_color = 0;
+    g_color = g_color / 2;
+    b_color = 255;
+  }
+  
+  // Parametrize colors
+  r_color = r_color * 1.0;
+  b_color = b_color * 0.5;
+  g_color = g_color * 0.75;
+
+  // RETURN
+  Serial.print(r_color);
+  Serial.print(" , ");
+  Serial.print(g_color);  
+  Serial.print(" , ");  
+  Serial.print(b_color);
+  Serial.println("");
+  analogWrite(red_pin, r_color);
+  analogWrite(green_pin, g_color);
+  analogWrite(blue_pin, b_color);
+  delay(250);
 }
